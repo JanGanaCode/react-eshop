84b6c5b53c3d79d292a9e584c27496e21935120f
Jan Gana <jan.gana@hiring-hub.com>
2020-02-28 15:59:15 +0000

    sign in page padding

--- src/pages/SignInAndSignUp/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/SignIn';
+import './signInAndSignUp.scss';
 
 const SignInAndSignUp = () => (
   <div className='sign-in-and-sign-up'>
